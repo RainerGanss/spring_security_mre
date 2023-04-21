2a66faddd507e3f579ed3c39719ff798141d0938
Rainer Ganß <rainer.ganss@4soft.de>
2023-04-21 11:43:20 +0200

    added example with jwt

---
+++ src/main/java/com/example/demo/DefaultSecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class DefaultSecurityConfiguration {
+
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer().jwt();
+    }
+}
